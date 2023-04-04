c7369e40f487a4bd1868ed15f407bdb0e65fd4a2
peanutyabing <sherry.liuyabing@gmail.com>
2023-04-04 17:14:11 +0800

    Fix filtering by year

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,3 +1,5 @@
+const { Op } = require("sequelize");
+
 class BaseController {
   constructor(model) {
     this.model = model;
@@ -8,13 +10,27 @@ class BaseController {
   getAll = async (req, res) => {
     try {
       const table = await this.model.findAll(
-        { where: req.query },
+        { where: this.processQueryParams(req.query) },
         { order: [["date"]] }
       );
       return res.json(table);
     } catch (err) {
       return res.status(400).json({ error: true, msg: err });
     }
+  };
+
+  processQueryParams = (queryParams) => {
+    const year = queryParams.date;
+    const nextYear = (parseInt(year) + 1).toString();
+    if (year) {
+      queryParams.date = {
+        [Op.between]: [
+          new Date(`${year}-01-01`).toISOString(),
+          new Date(`${nextYear}-01-01`).toISOString(),
+        ],
+      };
+    }
+    return queryParams;
   };
 }
 
